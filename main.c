ac4f51ed285f3f3dc154317cd60d1ac16d2fdb42
wa1tnr <wa1tnr@users.noreply.github.com>
2018-09-09 03:29:35 +0000

    On branch revision-00d-
    HELLO WORLD instantiated -- USART i/o works fine.
    
    Connected and talking to the CP2104 USART to USB bridge chip.
    
    This is in some sense a finished program.  It is the very
    basis of the CamelForth port given here, for the SAMD51.
    
    	modified:   main.c

--- main.c
+++ main.c
@@ -1,9 +1,12 @@
 #include <atmel_start.h>
+#include "driver_examples.h" // wa1tnr
 
 int main(void)
 {
 	/* Initializes MCU, drivers and middleware */
 	atmel_start_init();
+
+        USART_0_example(); // simple test of the USART.  wa1tnr
 
 	/* Replace with your application code */
 	while (1) {
