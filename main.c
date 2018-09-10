b0b50148ce11cb17ec9e26dc6936ea5a4474f717
wa1tnr <wa1tnr@users.noreply.github.com>
2018-09-10 23:38:18 +0000

    On branch camelforth_port_aa
    	modified:   main.c

--- main.c
+++ main.c
@@ -1,16 +1,18 @@
 #include <atmel_start.h>
-// #include "main_app.inc"
+#include "main_app.inc"
 
 
 // ///////////// temporary placement here  ///////////
 extern struct io_descriptor *io;
+extern void samplefunc(void);
 // ///////////// temporary placement here  ///////////
 
 int main(void) {
     atmel_start_init();
     SystemInit();
     /* Replace with your application code */
-    // main_app();
+    main_app();
+    samplefunc();
     io_write(io, (uint8_t *)
         "test 1 2 3", 10);
     while (1) {
