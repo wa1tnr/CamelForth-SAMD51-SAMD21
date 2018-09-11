2cc13ff17bfa64e4d72f0867bbe18ddce1fd5d32
wa1tnr <wa1tnr@users.noreply.github.com>
2018-09-11 18:57:55 +0000

    Well.  There was a lot of junk in this one.
    On branch camelforth
    
    	modified:   usart_gateway.c

--- src/cf/atsamdx1/usart_gateway.c
+++ src/cf/atsamdx1/usart_gateway.c
@@ -1,16 +1,17 @@
 /* wa1tnr - September, 2018 LGPL v2.1 */
 
 #include "atsamdx1_getkey.inc"
-#include "usart_ini.h"
-#include "atsamdx1.h"
+// #include "atsamdx1.h"
+// #include "usart_ini.h"
 
-// extern ch[];
-extern int read_ln;
-extern void queryKey(void);
 extern void camelforth(void);
-
 struct io_descriptor *io;
+// extern ch[];
+// extern int read_ln;
+// extern void queryKey(void);
 
+
+/*
 void usart_sync_get_desc(void) {
     usart_sync_get_io_descriptor(&USART_0, &io);
 }
@@ -18,15 +19,18 @@ void usart_sync_get_desc(void) {
 void usart_sync_enbl(void) {
     usart_sync_enable(&USART_0);
 }
+*/
 
 void USART_0_example_upper_camelforth(void) {
-    usart_sync_get_desc();
-    usart_sync_enbl();
+    // usart_sync_get_desc();
+    usart_sync_get_io_descriptor(&USART_0, &io);
+    // usart_sync_enbl();
+    usart_sync_enable(&USART_0);
 }
 
 void USART_0_example_lower_camelforth(void) {
     while(-1) { // endless loop
         camelforth(); //  CamelForth is invoked here
-        io_write(io, (uint8_t *) "dot.dot.dot\n", 12);
+        // io_write(io, (uint8_t *) "dot.dot.dot\n", 12);
     }
 }
