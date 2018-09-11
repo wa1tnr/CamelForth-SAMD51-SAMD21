83c7ab54ea7bc7decbffa1893d4bdfd59a0137b8
wa1tnr <wa1tnr@users.noreply.github.com>
2018-09-11 01:24:04 +0000

    On branch camelforth 	modified:   atsamdx1.h 	deleted:    term_emul.inc

--- src/cf/atsamdx1/atsamdx1.h
+++ src/cf/atsamdx1/atsamdx1.h
@@ -1,21 +1,20 @@
 #include <string.h>
 
 #define timeStamp(t,l) \
-    "Mon Sep 10 19:30:42 UTC 2018\n\n", 32
+    "Tue Sep 11 01:15:11 UTC 2018\n\n", 32
 // current target branch:
 #define branchStamp(b,l) \
-    "On branch excellent_progress_b      ", 36
+    "On branch camelforth    ", 24
+
 // #define branchStamp(b,l) "master                   ", 25
 
 #define stack_buffer_length 64
 
+/*
 extern unsigned int *psp;
 extern void itoa_write(void);
 extern int input_integer;
 extern void cmf_itoa(int n, char s[]);
-extern char print_string[stack_buffer_length];
-char print_string[stack_buffer_length];
-extern struct io_descriptor *io;
 extern void bold_italics_white(void);
 extern void italics(void);
 extern void bold_italics(void);
@@ -34,5 +33,9 @@ extern void _bkbar(void); // broken bar
 extern void _spc(void);
 extern void _cr(void);
 extern void cr(void); // why two cr?
+*/
+extern struct io_descriptor *io;
+extern char print_string[stack_buffer_length];
 extern void USART_0_example_upper_camelforth(void); // usart ini?
 extern void USART_0_example_lower_camelforth(void); // invoke camelforth?
+char print_string[stack_buffer_length];
