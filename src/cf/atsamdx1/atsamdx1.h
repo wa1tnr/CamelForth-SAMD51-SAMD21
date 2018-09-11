88aa51742528ab893b7e5e69f2eed78ce9de4f32
wa1tnr <wa1tnr@users.noreply.github.com>
2018-09-11 01:25:32 +0000

    On branch camelforth 	modified:   atsamdx1.h

--- src/cf/atsamdx1/atsamdx1.h
+++ src/cf/atsamdx1/atsamdx1.h
@@ -6,36 +6,12 @@
 #define branchStamp(b,l) \
     "On branch camelforth    ", 24
 
-// #define branchStamp(b,l) "master                   ", 25
+// #define branchStamp(b,l) "master    ", 10
 
 #define stack_buffer_length 64
 
-/*
-extern unsigned int *psp;
-extern void itoa_write(void);
-extern int input_integer;
-extern void cmf_itoa(int n, char s[]);
-extern void bold_italics_white(void);
-extern void italics(void);
-extern void bold_italics(void);
-extern void n1uro_italics(void);
-extern void fg_white_force_bold(void);
-extern void fg_white(void);
-extern void fg_yellow(void);
-extern void bg_black(void);
-extern void bg_blue(void);
-extern void bg_red(void);
-extern void color_reset(void);
-extern void color_bold(void);
-extern void _que(void);
-extern void _ok(void);
-extern void _bkbar(void); // broken bar
-extern void _spc(void);
-extern void _cr(void);
-extern void cr(void); // why two cr?
-*/
 extern struct io_descriptor *io;
 extern char print_string[stack_buffer_length];
-extern void USART_0_example_upper_camelforth(void); // usart ini?
-extern void USART_0_example_lower_camelforth(void); // invoke camelforth?
+extern void USART_0_example_upper_camelforth(void);
+extern void USART_0_example_lower_camelforth(void);
 char print_string[stack_buffer_length];
