84c0d53e0eb25a9461d32bdcff9d625dcb3e8969
wa1tnr <wa1tnr@users.noreply.github.com>
2018-09-11 20:32:47 +0000

    mass move cont On branch camelforth
    	new file:   driver_init.h

---
+++ boards/metro_m4_express/driver_init.h
@@ -0,0 +1,13 @@
+#ifndef DRIVER_INIT_INCLUDED
+#define DRIVER_INIT_INCLUDED
+
+// #include "atmel_start_pins.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+// void system_init(void);
+#ifdef __cplusplus
+}
+#endif
+#endif // DRIVER_INIT_INCLUDED
