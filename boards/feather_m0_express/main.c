4ff02c14266ecb06949cd28ca51c187622001c12
wa1tnr <wa1tnr@users.noreply.github.com>
2018-09-11 22:51:49 +0000

    This is that basis - the Hello World.
    On branch trial_merge_a
    	modified:   ../../main.c

--- boards/feather_m0_express/main.c
+++ boards/feather_m0_express/main.c
@@ -1,11 +1,20 @@
+// from:
+
+// Wed Aug 29 04:49:35 UTC 2018
+// On branch xKM_converser_d51-nn-
+
 #include <atmel_start.h>
+#include "driver_examples.h"
 
-int main(void)
-{
-	/* Initializes MCU, drivers and middleware */
-	atmel_start_init();
+int main(void) {
+    /* Initializes MCU, drivers and middleware */
+    atmel_start_init();
+    SystemInit();
+    // 8 MHz CPU
 
-	/* Replace with your application code */
-	while (1) {
-	}
+    USART_0_example(); // demo
+
+    /* Replace with your application code */
+    while (1) {
+    }
 }
