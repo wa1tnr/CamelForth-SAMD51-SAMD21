366beec7093ebabb2937bfeef4912035f367c1e3
wa1tnr <wa1tnr@users.noreply.github.com>
2018-09-11 19:13:19 +0000

    On branch camelforth
    More 'factoring' of C language coding practices. ;)
    
    	modified:   atsamdx1_getkey.inc
    	modified:   usart_gateway.c

--- src/cf/atsamdx1/usart_gateway.c
+++ src/cf/atsamdx1/usart_gateway.c
@@ -1,36 +1,19 @@
 /* wa1tnr - September, 2018 LGPL v2.1 */
 
+#include <atmel_start.h>
 #include "atsamdx1_getkey.inc"
-// #include "atsamdx1.h"
-// #include "usart_ini.h"
 
 extern void camelforth(void);
 struct io_descriptor *io;
-// extern ch[];
-// extern int read_ln;
-// extern void queryKey(void);
-
-
-/*
-void usart_sync_get_desc(void) {
-    usart_sync_get_io_descriptor(&USART_0, &io);
-}
-
-void usart_sync_enbl(void) {
-    usart_sync_enable(&USART_0);
-}
-*/
 
 void USART_0_example_upper_camelforth(void) {
-    // usart_sync_get_desc();
     usart_sync_get_io_descriptor(&USART_0, &io);
-    // usart_sync_enbl();
     usart_sync_enable(&USART_0);
 }
 
 void USART_0_example_lower_camelforth(void) {
-    while(-1) { // endless loop
-        camelforth(); //  CamelForth is invoked here
-        // io_write(io, (uint8_t *) "dot.dot.dot\n", 12);
+    while(-1) {
+        // io_write(io, (uint8_t *)"DEBUG entry point.", 18);
+        camelforth();
     }
 }
