45ad7c411869616030dc6c3054947f4f25eaf7e3
wa1tnr <wa1tnr@users.noreply.github.com>
2018-09-11 23:49:06 +0000

    Working CamelForth ATSAMD21G18A - 11 September 2018
    Adafruit Feather M0 Express
    
    de wa1tnr, Connecticut.
    
    On branch master
    
    	modified:   main.c

--- boards/feather_m0_express/main.c
+++ boards/feather_m0_express/main.c
@@ -1,20 +1,11 @@
-// from:
-
-// Wed Aug 29 04:49:35 UTC 2018
-// On branch xKM_converser_d51-nn-
-
 #include <atmel_start.h>
-#include "driver_examples.h"
+#include "main_app.inc"
 
 int main(void) {
-    /* Initializes MCU, drivers and middleware */
     atmel_start_init();
     SystemInit();
-    // 8 MHz CPU
-
-    USART_0_example(); // demo
-
     /* Replace with your application code */
+    main_app();
     while (1) {
     }
 }
