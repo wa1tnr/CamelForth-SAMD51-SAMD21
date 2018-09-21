90a06b04f4e75e520c8074af72bb4a165f9a84c3
wa1tnr <wa1tnr@users.noreply.github.com>
2018-09-21 10:59:58 +0000

    Working version - program build not tested; other files still need update or synchronization.
    On branch camelforth_testing
    
    	modified:   main.c

--- boards/metro_m4_usb/main.c
+++ boards/metro_m4_usb/main.c
@@ -1,13 +1,46 @@
 #include <atmel_start.h>
-#include "usb_start.h"
+#include "atmel_start_pins.h"
+// #include "main_app.inc"
+#include "pins.h"               // local
 
-int main(void)
-{
-	/* Initializes MCU, drivers and middleware */
-	atmel_start_init();
+void delay_some(void) {
+    for (volatile int i = 230001; i > 0; i--) {
+    }
+}
 
-	/* Replace with your application code */
-        cdcd_acm_example();
-	while (1) {
-	}
+void blink(void) {
+    reset_d13();
+    delay_some();
+    toggle_d13();
+    delay_some();
+    toggle_d13();
+}
+
+void blinks(void) {
+    for (int k = 3; k > 0; k--) {
+        for (int j = 3; j > 0; j--) {
+            delay_some();
+        }                       // offer a pattern
+        for (int i = 3; i > 0; i--) {
+            blink();
+        }
+    }
+}
+
+// int main(void)
+void main_cf(void) {
+    /* Initializes MCU, drivers and middleware */
+    atmel_start_init();
+    pins_setup();
+
+    /* Replace with your application code */
+    // blinks();
+    // main_app();
+    cdcd_acm_example();
+    while (1) {
+    }
+}
+
+int main(void) {
+    main_cf();
 }
